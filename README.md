f3ac28c789d29be91f0aed4e41c2d3a376cdee24
OSWOfficial <95764721+OSWOfficial@users.noreply.github.com>
2023-01-07 22:00:01 +0300

    DEGISIKLIK

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # C++ Türkçe
-Kullanmak için yeni bir C++ dosyası açın bu dosyaya ´´´c#include "cppturkce.hpp"´´´ yaz. Artık Hazırsın!
+Kullanmak için yeni bir C++ dosyası açın bu dosyaya #include "cppturkce.hpp" yaz. Artık Hazırsın!
---
+++ cppturkce.hpp
@@ -0,0 +1,23 @@
+// INCLUDES
+#include <iostream>
+
+// VOIDS
+void cppturkce() {
+    cout << "C++ Turkce Programlama Ogrenmek Isteyip Ingilizce Bilmeyen Turkler Icin Tasarlandi.";
+}
+
+// DEFINES
+#define yazdir cout
+#define girdi cin
+#define eger if
+#define sayi int
+#define ana main
+#define fonksiyonolustur void
+#define tanimla #define
+#define iceaktar #include
+#define ondaliklibuyuksayi double
+#define ondaliklisayi float
+#define arti +
+#define eksi -
+#define bolu /
+#define carpi *
